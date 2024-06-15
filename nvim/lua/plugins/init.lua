ea7e87bee607aaf91a400aa33661cc4d1397ad38
Mr TripleA <kraaakilo@gmail.com>
2024-06-15 15:27:14 +0100

    feat:Add nvim config.

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    'nvim-telescope/telescope.nvim',
+    config = function()
+      require('telescope').setup{
+        defaults = {
+          file_ignore_patterns = {"vendor/.*"}
+        }
+      }
+    end
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "intelephense",
+        "prettier",
+        "gopls",
+        "pyright",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "markdown",
+      },
+    },
+  },
+  {
+    'wakatime/vim-wakatime',
+    lazy = false
+  }
+}
