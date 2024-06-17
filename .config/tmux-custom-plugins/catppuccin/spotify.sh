92c934295e50d13379c7d4ea7721e7eb285c4907
Mr TripleA <kraaakilo@gmail.com>
2024-06-18 00:18:38 +0100

    add Quietwhen spotify is not playing.

--- .config/tmux-custom-plugins/catppuccin/spotify.sh
+++ .config/tmux-custom-plugins/catppuccin/spotify.sh
@@ -1,13 +1,13 @@
 show_spotify() { # This function name must match the module name!
-  local index icon color text module
+	local index icon color text module
 
-  index=$1
+	index=$1
 
-  icon="$(  get_tmux_option "@catppuccin_spotify_icon"  ""           )"
-  color="$( get_tmux_option "@catppuccin_spotify_color" "#1DB954" )"
-  text="$(  get_tmux_option "@catppuccin_spotify_text"  "#(playerctl --player=spotify metadata --format '{{artist}} - {{title}}')" )"
+	icon="$(get_tmux_option "@catppuccin_spotify_icon" " ")"
+	color="$(get_tmux_option "@catppuccin_spotify_color" "#1DB954")"
+	text="$(get_tmux_option "@catppuccin_spotify_text" "#(~/.config/polybar/scripts/get_spotify_status.sh)")"
 
-  module=$( build_status_module "$index" "$icon" "$color" "$text" )
+	module=$(build_status_module "$index" "$icon" "$color" "$text")
 
-  echo "$module"
+	echo "$module"
 }
